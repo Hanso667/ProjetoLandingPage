2445477a64f26165f4ea41310098862f68013893
Hanso667 <fabriciolacerdamoraes2005@gmail.com>
2025-05-29 16:43:28 -0300

    third commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -18,3 +18,12 @@ function MenuOpen() {
         mnLogin.classList.add("open")
     }
 }
+
+let imgs = ['Celeste', 'DarkSouls3', 'HollowKnight', 'Minecraft', 'Portal2', 'RedDead2', 'StardewValley', 'Zelda']
+
+imgs.forEach(Imagem => {
+    let Card = document.createElement('img'); // use 'img', não 'image'
+    Card.src = `src/img/games/${Imagem}.png`;  // caminho relativo a partir de /script/script.js
+    Card.className = 'Teste';
+    document.getElementById('card-View').appendChild(Card);
+});
